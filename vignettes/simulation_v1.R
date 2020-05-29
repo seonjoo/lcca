2b449a8c9ab96df80e3ef25759bc444a48a62900
Jongwoo Choi <jongwoo@jongwoo.home>
2020-05-29 12:49:24 -0400

    first simulation version 1

--- vignettes/simulation_v1.R
+++ vignettes/simulation_v1.R
@@ -5,6 +5,7 @@ knitr::opts_chunk$set(echo = TRUE)
 library(lcca)
 library(MASS)
 library(gplots)
+library(tidyverse)
 
 sim_lcca <- function(I, r){
   ccor.list = rep(0, 100)
@@ -13,6 +14,11 @@ sim_lcca <- function(I, r){
   xcv_x1.array = array(0, dim=c(144,1,100))
   xcv_y0.array = array(0, dim=c(81,1,100))
   xcv_y1.array = array(0, dim=c(81,1,100))
+
+  cv_x1s = rep(0,100)
+  cv_x1s = rep(0,100)
+  cv_y0s = rep(0,100)
+  cv_y1s = rep(0,100)
 
   set.seed(12345678)
 
@@ -67,64 +73,191 @@ sim_lcca <- function(I, r){
     xcv_y0.array[,,i] <- matrix(re$xcv_y0, nrow=81, ncol=1)
     xcv_y1.array[,,i] <- matrix(re$xcv_y1, nrow=81, ncol=1)
 
-    xcv_x0s <- abs(cor(xcv_x0.array[,,i], phix0)[,1])
-    xcv_x1s <- abs(cor(xcv_x1.array[,,i], phix1)[,1])
+    cv_x1s[i] <- abs(cor(xcv_x0.array[,,i], phix0)[,1])
+    cv_x1s[i] <- abs(cor(xcv_x1.array[,,i], phix1)[,1])
 
-    xcv_y0s <- abs(cor(xcv_y0.array[,,i], phiy0)[,1])
-    xcv_y1s <- abs(cor(xcv_y1.array[,,i], phiy1)[,1])
+    cv_y0s[i] <- abs(cor(xcv_y0.array[,,i], phiy0)[,2])
+    cv_y1s[i] <- abs(cor(xcv_y1.array[,,i], phiy1)[,2])
   }
 
   out=list(ccor.dim.list=ccor.dim.list, ccor.list=ccor.list,
-           xcv_x0s=xcv_x0s,
-           xcv_x1s=xcv_x1s,
-           xcv_y0s=xcv_y0s,
-           xcv_y1s=xcv_y1s)
+           cv_x1s=cv_x1s,
+           cv_x1s=cv_x1s,
+           cv_y0s=cv_y0s,
+           cv_y1s=cv_y1s)
   return(out)
 }
 
+get_bias = function(estimate, truth) {
+  mean(estimate) - truth
+}
 
 ## I=50, r=0.8
+sim11 <- sim_lcca(I=50, r=0.8)
+get_bias(mean(sim11$ccor.list), 0.8)
+
 ## I=50, r=0.5
+sim12 <- sim_lcca(I=50, r=0.5)
+get_bias(mean(sim12$ccor.list), 0.5)
+
 ## I=50, r=0.3
+sim13 <- sim_lcca(I=50, r=0.3)
+get_bias(mean(sim13$ccor.list), 0.3)
+
 ## I=50, r=0.1
+sim14 <- sim_lcca(I=50, r=0.1)
+get_bias(mean(sim14$ccor.list), 0.1)
 
 
 ## I=100, r=0.8
-sim1 <- sim_lcca(I=100, r=0.8)
-
-# Dimensions
-mean(sim1$ccor.dim.list)
-sd(sim1$ccor.dim.list)
-
-# CCs
-mean(sim1$ccor.list)
-sd(sim1$ccor.list)
-
-# CVs
-
-
-
-
+sim15 <- sim_lcca(I=100, r=0.8)
+get_bias(mean(sim15$ccor.list), 0.8)
 ## I=100, r=0.5
-sim2 <- sim_lcca(I=100, r=0.5, varthresh=0.97)
-
+sim16 <- sim_lcca(I=100, r=0.5)
+get_bias(mean(sim16$ccor.list), 0.5)
 ## I=100, r=0.3
-sim3 <- sim_lcca(I=100, r=0.2, varthresh=0.97)
-
+sim17 <- sim_lcca(I=100, r=0.3)
+get_bias(mean(sim17$ccor.list), 0.3)
 ## I=100, r=0.1
-sim4 <- sim_lcca(I=100, r=0.8, varthresh=0.95)
+sim18 <- sim_lcca(I=100, r=0.1)
+get_bias(mean(sim18$ccor.list), 0.1)
 
 
 ## I=200, r=0.8
+sim19 <- sim_lcca(I=200, r=0.8)
+get_bias(mean(sim19$ccor.list), 0.8)
 ## I=200, r=0.5
+sim110 <- sim_lcca(I=200, r=0.5)
+get_bias(mean(sim110$ccor.list), 0.5)
 ## I=200, r=0.3
+sim111 <- sim_lcca(I=200, r=0.3)
+get_bias(mean(sim111$ccor.list), 0.3)
 ## I=200, r=0.1
+sim112 <- sim_lcca(I=200, r=0.1)
+get_bias(mean(sim112$ccor.list), 0.1)
 
 
 ## I=400, r=0.8
+sim113 <- sim_lcca(I=400, r=0.8)
+get_bias(mean(sim113$ccor.list), 0.8)
 ## I=400, r=0.5
+sim114 <- sim_lcca(I=400, r=0.5)
+get_bias(mean(sim114$ccor.list), 0.5)
 ## I=400, r=0.3
+sim115 <- sim_lcca(I=400, r=0.3)
+get_bias(mean(sim115$ccor.list), 0.3)
 ## I=400, r=0.1
+sim116 <- sim_lcca(I=400, r=0.1)
+get_bias(mean(sim116$ccor.list), 0.1)
 
 
 
+dd <- data.frame(ccors = c(sim11$ccor.list, sim12$ccor.list, sim13$ccor.list, sim14$ccor.list,
+                           sim15$ccor.list, sim16$ccor.list, sim17$ccor.list, sim18$ccor.list,
+                           sim19$ccor.list, sim110$ccor.list, sim111$ccor.list, sim112$ccor.list,
+                           sim113$ccor.list, sim114$ccor.list, sim115$ccor.list, sim116$ccor.list),
+                 r = factor(rep(c(0.8,0.5,0.3,0.1), each=100)),
+                 i = factor(rep(c(50,100,200,400), each=400)))
+
+ggplot(dd , aes(x=r, y=ccors)) +
+  geom_boxplot() +
+  scale_y_continuous(name = "Estimated Canonical Correlations",
+                     breaks = seq(0,1,.2),
+                     limits=c(0, 1)) +
+  scale_x_discrete(name = "Canonical Correlations") +
+  ggtitle("Boxplot of estimated canonical correlations") +
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
+        text = element_text(size = 12, family = "Tahoma"),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 11)) +
+  facet_grid(. ~ i)
+
+
+dd <- data.frame(cv_x0 = c(sim11$cv_x0s, sim12$cv_x0s, sim13$cv_x0s, sim14$cv_x0s,
+                           sim15$cv_x0s, sim16$cv_x0s, sim17$cv_x0s, sim18$cv_x0s,
+                           sim19$cv_x0s, sim110$cv_x0s, sim111$cv_x0s, sim112$cv_x0s,
+                           sim113$cv_x0s, sim114$cv_x0s, sim115$cv_x0s, sim116$cv_x0s),
+                 r = factor(rep(c(0.8,0.5,0.3,0.1), each=100)),
+                 i = factor(rep(c(50,100,200,400), each=400)))
+
+ggplot(dd , aes(x=r, y=cv_x0)) +
+  geom_boxplot() +
+  scale_y_continuous(name = "Estimated CVs * True CVs X intercept",
+                     breaks = seq(0,1,.2),
+                     limits=c(0, 1)) +
+  scale_x_discrete(name = "Canonical Correlations") +
+  ggtitle("Boxplot of Inner product of Estimated CVs and True CVs X intercept") +
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
+        text = element_text(size = 12, family = "Tahoma"),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 11)) +
+  facet_grid(. ~ i)
+
+dd <- data.frame(cv_x1 = c(sim11$cv_x1s, sim12$cv_x1s, sim13$cv_x1s, sim14$cv_x1s,
+                           sim15$cv_x1s, sim16$cv_x1s, sim17$cv_x1s, sim18$cv_x1s,
+                           sim19$cv_x1s, sim110$cv_x1s, sim111$cv_x1s, sim112$cv_x1s,
+                           sim113$cv_x1s, sim114$cv_x1s, sim115$cv_x1s, sim116$cv_x1s),
+                 r = factor(rep(c(0.8,0.5,0.3,0.1), each=100)),
+                 i = factor(rep(c(50,100,200,400), each=400)))
+
+ggplot(dd , aes(x=r, y=cv_x1)) +
+  geom_boxplot() +
+  scale_y_continuous(name = "Estimated CVs * True CVs X slope",
+                     breaks = seq(0,1,.2),
+                     limits=c(0, 1)) +
+  scale_x_discrete(name = "Canonical Correlations") +
+  ggtitle("Boxplot of Inner product of Estimated CVs and True CVs X slope") +
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
+        text = element_text(size = 12, family = "Tahoma"),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 11)) +
+  facet_grid(. ~ i)
+
+
+
+dd <- data.frame(cv_y0 = c(sim11$cv_y0s, sim12$cv_y0s, sim13$cv_y0s, sim14$cv_y0s,
+                           sim15$cv_y0s, sim16$cv_y0s, sim17$cv_y0s, sim18$cv_y0s,
+                           sim19$cv_y0s, sim110$cv_y0s, sim111$cv_y0s, sim112$cv_y0s,
+                           sim113$cv_y0s, sim114$cv_y0s, sim115$cv_y0s, sim116$cv_y0s),
+                 r = factor(rep(c(0.8,0.5,0.3,0.1), each=100)),
+                 i = factor(rep(c(50,100,200,400), each=400)))
+
+ggplot(dd , aes(x=r, y=cv_y0)) +
+  geom_boxplot() +
+  scale_y_continuous(name = "Estimated CVs * True CVs Y intercept",
+                     breaks = seq(0,1,.2),
+                     limits=c(0, 1)) +
+  scale_x_discrete(name = "Canonical Correlations") +
+  ggtitle("Boxplot of Inner product of Estimated CVs and True CVs Y intercept") +
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
+        text = element_text(size = 12, family = "Tahoma"),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 11)) +
+  facet_grid(. ~ i)
+
+dd <- data.frame(cv_y1 = c(sim11$cv_y1s, sim12$cv_y1s, sim13$cv_y1s, sim14$cv_y1s,
+                           sim15$cv_y1s, sim16$cv_y1s, sim17$cv_y1s, sim18$cv_y1s,
+                           sim19$cv_y1s, sim110$cv_y1s, sim111$cv_y1s, sim112$cv_y1s,
+                           sim113$cv_y1s, sim114$cv_y1s, sim115$cv_y1s, sim116$cv_y1s),
+                 r = factor(rep(c(0.8,0.5,0.3,0.1), each=100)),
+                 i = factor(rep(c(50,100,200,400), each=400)))
+
+ggplot(dd , aes(x=r, y=cv_y1)) +
+  geom_boxplot() +
+  scale_y_continuous(name = "Estimated CVs * True CVs Y slope",
+                     breaks = seq(0,1,.2),
+                     limits=c(0, 1)) +
+  scale_x_discrete(name = "Canonical Correlations") +
+  ggtitle("Boxplot of Inner product of Estimated CVs and True CVs Y slope") +
+  theme_bw() +
+  theme(plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
+        text = element_text(size = 12, family = "Tahoma"),
+        axis.title = element_text(face="bold"),
+        axis.text.x=element_text(size = 11)) +
+  facet_grid(. ~ i)
+
+save.image()
